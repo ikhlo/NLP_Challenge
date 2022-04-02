65b80bbfd9520953da100e7171a9f9a5a810ac5b
ikhlo <ikhlassyo@gmail.com>
2022-04-02 18:52:40 +0200

    modifying utils

--- utils.py
+++ utils.py
@@ -14,7 +14,8 @@ def process_json(json_data):
   for individual in json_data:
     for day_of_speech in individual['speech']:
       for bank in day_of_speech.keys():
-        day_of_speech[bank] = '' if len(day_of_speech[bank])==0 else day_of_speech[bank][0].strip('\" ')
+        day_of_speech[bank] = '' if len(day_of_speech[bank])==0\
+                else day_of_speech[bank][0].strip('\" ')
 
 
 def get_list_speeches(json_data, bank='FED'):
@@ -26,9 +27,9 @@ def get_list_speeches(json_data, bank='FED'):
     (String) bank: The name of the bank's speeches we are working on.
 
   Return:
-    A List containing unique speechs for the chosen bank. 
+    A List of size N containing unique speechs for the chosen bank.
   """
-  N = len(data)
+  N = len(json_data)
   T = 20
     
   speech_set = set()
@@ -36,7 +37,7 @@ def get_list_speeches(json_data, bank='FED'):
 
   for sample in range(N):
     for day in range(T):
-      speech = data[sample]['speech'][day][bank]
+      speech = json_data[sample]['speech'][day][bank]
       if speech not in speech_set:
         speech_set.add(speech)
         speech_list.append(speech)
@@ -44,83 +45,82 @@ def get_list_speeches(json_data, bank='FED'):
   return speech_list
 
 
-def load_data(path_to_data, get_dev_data = False):
-    train_vix_json = Path(path_to_data) / 'train/VIX_1w.json'
-    train_eur_json = Path(path_to_data) / 'train/EURUSDV1M_1w.json'
+def get_languages(speech_list):
+  """
+  For a list of speechs, define the language of each speech and store it inside
+  a numpy array.
+  
+  Args:
+    (List) speech_list: All N-unique speeches. 
 
-    with open(train_vix_json, 'r') as json_file:
-    train_vix = json.load(json_file)
+  Return:
+    A (N, ) numpy.ndarray with object dtype containing at index i the language
+    of the i-th speech of the speeches' list. 
+  """
+  lang = []
+  for speech in speech_list:
+    if speech = '':
+      lang.append('en')
+    else:
+      lang.append(detect(speech))
+  return np.array(lang)
 
-    with open(train_eur_json, 'r') as json_file:
-    train_eur = json.load(json_file)
 
-    if get_dev_data:
-        val_vix_json = Path(path_to_data) / 'dev/VIX_1w.json'
-        val_eur_json = Path(path_to_data) / 'dev/EURUSDV1M_1w.json'
+def build_dataset(json_data, ecb_list, fed_list, labels=True):
+  """
+  A function to build dataset from JSON dict and lists of speeches.
+  
+  Args:
+    (Dict) json_data: The JSON dict data.
+    (List) ecb_list: All K-unique speeches from ECB bank.
+    (List) fed_list: All L-unique speeches from FED bank.
+    (Boolean) labels: If True, return the labels of each individual.
 
-        with open(val_vix_json, 'r') as json_file:
-        valid_vix = json.load(json_file)
+  Return:
+    (numpy.ndarray) X:
+    An array of size (N, 3, 20). For each sample, there is the stock index
+    values, the speech index for ECB and FED bank on 20 days.
 
-        with open(val_eur_json, 'r') as json_file:
-        valid_eur = json.load(json_file)
-        
-        return train_vix, valid_vix, train_eur, valid_eur
-    
-    return train_vix, train_eur
+    (numpy.ndarray) y_clf:
+    An array of size (N, 1). It contains the classification label for each
+    sample.
 
-def get_list_speeches(data):
-    N = len(data)
-    T = 20
-    
-    ecb_speech = ['<NA>']
-    fed_speech = ['<NA>']
-    banks = ['ECB', 'FED']
+    (numpy.ndarray) y_reg:
+    An array of size (N, 1). It contains the regression label for each sample.
 
-    for sample in range(N):
-      for day in range(T):
-        for b in banks:
-          speech = data[sample]['speech'][day][b]
-          if (len(speech) != 0) and (speech not in eval(b.lower()+"_speech")):
-            eval(b.lower()+"_speech").append(speech[0])
-    return fed_speech, ecb_speech
+    where N is the number of samples in the JSON dict data.
+  """
+  N = len(json_data)
+  T = 20
+  banks = ['ECB', 'FED']
 
-def build_dataset(data_vix, data_eur, labels=True):
-    N = len(data_vix)
-    T = 20
-    banks = ['ECB', 'FED']
-    X_stock = np.empty((N, 2))
-    X_speech = np.empty((N, 2))
-    y = np.empty((N, 4))
-    
-    fed_speech, ecb_speech = get_list_speeches(data_vix)
+  X = np.empty((N, 3, 20))
+  if labels:
+    y_clf = np.empty((N, 1))
+    y_reg = np.empty((N, 1))
 
-    for i in range(len(data_vix)):
-        idx_ecb = []
-        idx_fed = []
-        for day in range(T):
-          for b in banks:
-            speech = data_vix[i]['speech'][day][b]
-            if (len(speech) == 0) or (speech[0] not in eval(b.lower()+"_speech")):
-              eval("idx_"+b.lower()).append(0)
-            else:
-              eval("idx_"+b.lower()).append(eval(b.lower()+"_speech").index(speech[0]))
+  ecb_dict = {speech : i for i, speech in enumerate(ecb_list)}
+  fed_dict = {speech : i for i, speech in enumerate(fed_list)}
 
-        X_stock[i, 0] = np.array(data_vix[i]['stock'])
-        X_stock[i, 1] = np.array(data_eur[i]['stock'])
-        X_speech[i, 0] = np.array(idx_fed)
-        X_speech[i, 1] = np.array(idx_ecb)
-        
-        if labels:
-            y[i, 0] = data_vix[i]['target_reg']
-            y[i, 1] = data_vix[i]['target_classif']
-            y[i, 2] = data_eur[i]['target_reg']
-            y[i, 3] = data_eur[i]['target_classif']
-    
-    return (X_stock, X_speech), y
+  for i in range(len(json_data)):
+    idx_ecb = []
+    idx_fed = []
+    for day in range(T):
+      for b in banks:
+        speech = json_data[i]['speech'][day][b]
+        if speech in eval(b.lower()+"_dict").keys():
+          eval("idx_"+b.lower()).append(eval(b.lower()+"_dict")[speech])
+        else:
+          eval("idx_"+b.lower()).append(eval(b.lower()+"_dict")[''])
 
-def detect_language(list_of_speechs):
-    lang = []
-    for speech in speech_list:
-        lang.append(detect(speech))
-    lang[0] = 'en'
-    return lang
+    X[i, 0, :] = np.array(json_data[i]['stock'])
+    X[i, 1, :] = np.array(idx_ecb)
+    X[i, 2, :] = np.array(idx_fed)
+      
+    if labels:
+      y_clf[i, 0] = data[i]['target_classif']
+      y_reg[i, 0] = data[i]['target_reg'] 
+
+    if labels : return X, y_clf, y_reg
+    else : return X
+
